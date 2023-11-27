5794b9418aefb85faa1b4b809d12d4c4820f4701
chrenx <chrenx@umich.edu>
2023-11-27 14:19:03 -0500

    update from chrenx

--- build/IG-generator/CMakeFiles/install-IGG.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-IGG"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-IGG.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
