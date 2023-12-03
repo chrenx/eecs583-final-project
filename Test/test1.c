861c1a1ceb0e56d2979453e0fea6ee925badf313
chrenx <chrenx@umich.edu>
2023-12-03 12:46:40 -0500

    update from chrenx

--- Test/test1.c
+++ Test/test1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a = 3;
-    for (int i = 0; i < 10; ++i) {
-        a += i;
-    }
-    printf("%d\n", a);
-    return 0;
+  int a = 3;
+  for (int i = 0; i < 10; ++i) {
+    a += i;
+  }
+  printf("%d\n", a);
+  return 0;
 }
