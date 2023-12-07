6eaca9e4459535fd970930bb43a554101a3bf4d9
chrenx <chrenx@umich.edu>
2023-12-07 15:07:35 -0500

    update from chrenx

--- machine-function-pass/X86TargetMachine.cpp
+++ machine-function-pass/X86TargetMachine.cpp
@@ -12,7 +12,7 @@
 // #include "llvm/Target/TargetRegisterInfo.h"
 #include "llvm/CodeGen/VirtRegMap.h"
 #include "llvm/CodeGen/Spiller.h"
-// #include "RegisterCoalescer.h"
+// #include "llvm/CodeGen/RegisterCoalescer.h"
 #include "llvm/CodeGen/LiveIntervals.h"
 #include "llvm/CodeGen/LiveInterval.h"
 #include "llvm/CodeGen/LiveStacks.h"
@@ -50,7 +50,7 @@ std::map<unsigned, unsigned> NumPhysicalRegisters; // type: num of physical regi
 
 class X86IGGenerator : public MachineFunctionPass {
   private:
-    bool belongToSameClass(Register &reg1, Register &reg2);
+    bool belongToSameClass(Register reg1, Register reg2);
 
   public:
     static char ID;
@@ -69,7 +69,7 @@ class X86IGGenerator : public MachineFunctionPass {
       initializeX86IGGeneratorPass(*PassRegistry::getPassRegistry());
       initializeSlotIndexesPass(*PassRegistry::getPassRegistry());
       initializeLiveIntervalsPass(*PassRegistry::getPassRegistry());
-      initializeRegisterCoalescerPass(*PassRegistry::getPassRegistry());
+      // initializeRegisterCoalescerPass(*PassRegistry::getPassRegistry());
       initializeLiveStacksPass(*PassRegistry::getPassRegistry());
       initializeMachineLoopInfoPass(*PassRegistry::getPassRegistry());
       initializeVirtRegMapPass(*PassRegistry::getPassRegistry());
@@ -83,7 +83,7 @@ class X86IGGenerator : public MachineFunctionPass {
       AU.addRequired<SlotIndexes>();
       AU.addPreserved<SlotIndexes>();
       AU.addRequired<LiveIntervals>();
-      //AU.addRequired<RegisterCoalescer>();
+      // AU.addRequired<RegisterCoalescer>();
       AU.addRequired<LiveStacks>();
       AU.addPreserved<LiveStacks>();
       AU.addRequired<MachineLoopInfo>();
@@ -96,6 +96,7 @@ class X86IGGenerator : public MachineFunctionPass {
     }
     
     bool runOnMachineFunction(MachineFunction &mf) override;
+    void printFunction();
     void buildInterferenceGraph();
     void printInterferenceGraph();
 
@@ -108,10 +109,13 @@ char X86IGGenerator::ID = 0;
 //=============================== Private ====================================//
 
 // Check whether two VRs are of the same type
-bool X86IGGenerator::belongToSameClass(Register &reg1, Register &reg2) {
+bool X86IGGenerator::belongToSameClass(Register reg1, Register reg2) {
+  // LOG("这里1"); LOG("\n");
 	const TargetRegisterClass *class1 = MF->getRegInfo().getRegClass(reg1);
+  // LOG("这里2"); LOG("\n");
 	const TargetRegisterClass *class2 = MF->getRegInfo().getRegClass(reg2);
-
+  // LOG("这里3"); LOG("\n");
+  // LOG("\n----------------------\n")
 	return class1 == class2;
 }
 
@@ -121,30 +125,65 @@ bool X86IGGenerator::belongToSameClass(Register &reg1, Register &reg2) {
 //Builds Interference Graphs, one for each register type
 void X86IGGenerator::buildInterferenceGraph() {
   LOG("Running buildInterferenceGraph()"); LOG("\n");
-	InterferenceGraph.resize(mri->getNumVirtRegs(), 
-                           std::vector<unsigned>(mri->getNumVirtRegs()));
   LOG("   # of virtual registers: "); LOG(mri->getNumVirtRegs()); LOG("\n");
+
+  std::vector<MachineInstr *> vr_def;
+  std::vector<Register> virtual_registers;
 	for (unsigned i = 0; i < mri->getNumVirtRegs(); i++) {
 		Register ii = Register::index2VirtReg(i);
+    if  (mri->getVRegDef(ii) == nullptr || mri->reg_nodbg_empty(ii) || !ii.isVirtual()) {
+      continue;
+    } else {
+      mri->getVRegDef(ii)->print(errs());
+      errs() << "  :" << mri->getVRegName(ii) << "\n";
+      virtual_registers.push_back(ii);
+      vr_def.push_back(mri->getVRegDef(ii));
+    }
+	}
+
+  // LOG("查看vr\n");
+  // for (int i = 0; i < (int)virtual_registers.size(); i++) {
+  //   errs() << virtual_registers[i].id() << "\n";
+  // }
+  // LOG("\n");
+  
+  InterferenceGraph.resize(virtual_registers.size(), 
+                           std::vector<unsigned>(virtual_registers.size()));
+
+  // build interference graph by checking overlapping live interval
+  for (unsigned i = 0; i < virtual_registers.size(); i++) {
+    Register ii = virtual_registers[i];
     if (LI->hasInterval(ii)) {
 			if(ii.isPhysical()) 
 				continue;
 			const LiveInterval &li = LI->getInterval(ii); 
 			// InterferenceGraph[i][i] = 1;  不给自己有interference
-			for (unsigned j = i + 1; j < mri->getNumVirtRegs(); ++j) {
-				Register jj = Register::index2VirtReg(j);
+			for (unsigned j = i + 1; j < virtual_registers.size(); ++j) {
+				Register jj = virtual_registers[j];
         if (LI->hasInterval(jj)) {
-					if(jj.isPhysical() || !belongToSameClass(ii, jj))
+					if(jj.isPhysical() || !belongToSameClass(ii, jj)) {
+            // LOG("\n+++++++++++++++++++++++++\n")
 						continue;
+          }
 					const LiveInterval &li2 = LI->getInterval(jj);
+          // LOG("++++++++++++++++++++\n")
+          // li2.print(errs());
+          // LOG("\n++++++++++++++++++++")
 					if (li.overlaps(li2)) {
-						InterferenceGraph[ii][jj] = 1;
-						InterferenceGraph[jj][ii] = 1;
+						InterferenceGraph[i][j] = 1;
+						InterferenceGraph[j][i] = 1;
+            LOG("这里有interference:\n");
+            vr_def[i]->print(errs());
+            vr_def[j]->print(errs());
+            LOG("\n");
 					}
 				}
 			}
 		}	
-	}
+  }
+
+  LOG("virtual reg: "); LOG(virtual_registers.size()); LOG("\n");
+
 
   // print 2d vector for debug
   for (unsigned int i = 0; i < InterferenceGraph.size(); i++) {
@@ -194,6 +233,14 @@ void X86IGGenerator::printInterferenceGraph() {
 	fclose(fp);
 }
 
+void X86IGGenerator::printFunction() {
+  // Function &F = MF->getFunction();
+  // FILE* fp = fopen("../Test/machine_instruction.txt", "w");
+  for (MachineBasicBlock &BB : *MF) {
+    errs() << BB << "\n";
+  }
+}
+
 // Run machine function pass
 bool X86IGGenerator::runOnMachineFunction(MachineFunction &mf) {
   LOG("\nRunning IGGenerator On function: "); LOG(mf.getFunction().getName()); LOG("\n");
@@ -202,26 +249,27 @@ bool X86IGGenerator::runOnMachineFunction(MachineFunction &mf) {
 	// TRI = TM->getRegisterInfo();
 	mri = &MF->getRegInfo(); 
 	LI = &getAnalysis<LiveIntervals>();
+  LOG("\n++++++++++++++++++++++++++++++++\n");
+  printFunction();
+  LOG("++++++++++++++++++++++++++++++++\n");
 	buildInterferenceGraph();
 	printInterferenceGraph();
 	InterferenceGraph.clear( );
 	return true;
 }
 
-INITIALIZE_PASS(X86IGGenerator, "x86-ig-generator", X86_IG_GENERATOR_PASS_NAME,
-                true, true)
+INITIALIZE_PASS_BEGIN(X86IGGenerator, "x86-ig-generator", X86_IG_GENERATOR_PASS_NAME, true, true)
 // INITIALIZE_PASS_DEPENDENCY(LiveDebugVariables)
-// INITIALIZE_PASS_DEPENDENCY(SlotIndexes)
-// INITIALIZE_PASS_DEPENDENCY(LiveIntervals)
+INITIALIZE_PASS_DEPENDENCY(SlotIndexes)
+INITIALIZE_PASS_DEPENDENCY(LiveIntervals)
 // INITIALIZE_PASS_DEPENDENCY(RegisterCoalescer)
-// INITIALIZE_PASS_DEPENDENCY(LiveStacks)
+INITIALIZE_PASS_DEPENDENCY(LiveStacks)
 // INITIALIZE_PASS_DEPENDENCY(AAResultsWrapperPass)
 // INITIALIZE_PASS_DEPENDENCY(MachineDominatorTree)
-// INITIALIZE_PASS_DEPENDENCY(MachineLoopInfo)
-// INITIALIZE_PASS_DEPENDENCY(VirtRegMap)
+INITIALIZE_PASS_DEPENDENCY(MachineLoopInfo)
+INITIALIZE_PASS_DEPENDENCY(VirtRegMap)
 // INITIALIZE_PASS_DEPENDENCY(LiveRegMatrix)
-// INITIALIZE_PASS_END(X86IGGenerator, "x86-ig-generator", X86_IG_GENERATOR_PASS_NAME,
-//                 true, true)
+INITIALIZE_PASS_END(X86IGGenerator, "x86-ig-generator", X86_IG_GENERATOR_PASS_NAME, true, true)
 
 FunctionPass *llvm::createX86IGGeneratorPass() { 
   return new X86IGGenerator(); 
